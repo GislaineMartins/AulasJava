c71ed52731ab3128314f3e1c4a49c0d8a7f39299
Gislaine Amancio <gimcjf@gmail.com>
2021-09-10 17:01:20 -0300

    A2- Tipos numericos

---
+++ src/modulo1/aula2/a2_2_tipobyte.java
@@ -0,0 +1,16 @@
+package modulo1.aula2;
+
+public class a2_2_tipobyte {
+
+    public static void main(String[] args) {
+        soma(10,15);
+    }
+
+    public static int  soma(int n1, int n2){
+        int resultado = n1 + n2;
+        return  resultado;
+
+    }
+
+
+}
---
+++ src/modulo1/aula2/a2_3_short.java
@@ -0,0 +1,8 @@
+package modulo1.aula2;
+
+public class a2_3_short {
+public static void main(){
+    short menor_valor = Short.MIN_VALUE;
+    short maior_valor = Short.MAX_VALUE;
+}
+}
