c47484db642c6459ff5b2b6157c423625a51ef78
Gislaine Amancio <gimcjf@gmail.com>
2021-09-17 14:18:25 -0300

    Alterações feita no código

---
+++ src/modulo1/aula6/a6_1_semanal.java
@@ -0,0 +1,27 @@
+package modulo1.aula6;
+
+public class a6_1_semanal {
+
+    public static void main(String[] args) {
+        String nome = "";
+        int numero = 10;
+        //byte, short, long =
+        //float, double = 0.0;
+        int contador = 0;
+
+/*      while(contador <= 10){
+             System.out.println(contador);
+        contador++;
+        }*/
+
+        int cont = 0;
+        while (cont <= 10) {
+            int soma = cont + 1;
+            System.out.println(soma);
+            cont++;
+
+        }
+
+
+    }
+}
