5d094ab393a69aa5f945b33cf787e57616f1796f
Gislaine Amancio <gimcjf@gmail.com>
2021-09-21 16:50:18 -0300

    Atividades sobre metodos

--- src/atividadesPraticas/EstruturaFor03.java
+++ src/atividadesPraticas/EstruturaFor03.java
@@ -5,21 +5,22 @@ package atividadesPraticas;
         a uma taxa de juros de 2% ao mês. Utilize o laço FOR para imprimir o valor do retorno mês a mês.*/
 
 
+import java.text.DecimalFormat;
 import java.util.Scanner;
 
 public class EstruturaFor03 {
     public static void main(String[] args) {
 
-      double inicial = 5000, tempo = 24, taxa = 2;
+      double inicial = 5000, tempo = 24, taxa = 0.02, m=0;
 
-      double M =0, total =0;
-
-        for (int i = 0; i <24 ; i++) {
-            M = (total + (inicial/ 100)*2);
-            total = M + inicial;
-            System.out.println("mes "+i + " total = "+M);
+      for (int i = 0; i <=tempo; i++) {
+           m = inicial * taxa;
+           inicial += m;
+            System.out.println("mes: "+ i+ " R$" + inicial);
 
         }
+
+
 
 
     }
