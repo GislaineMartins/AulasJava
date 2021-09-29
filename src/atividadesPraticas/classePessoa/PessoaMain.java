c8c04f360891a1a30c0a992e17bc80088d21167d
Gislaine Amancio <gimcjf@gmail.com>
2021-09-29 14:47:48 -0300

    impressão dos dados de duas pessoas

--- src/atividadesPraticas/classePessoa/PessoaMain.java
+++ src/atividadesPraticas/classePessoa/PessoaMain.java
@@ -12,5 +12,15 @@ public class PessoaMain {
         p2.nome = "Rodrigo";
         p2.sobrenome = "Souza";
         p2.idade = 28;
+
+        System.out.println("***********Dados pessoa 1************");
+        System.out.println("Nome: "+p1.nome);
+        System.out.println("Nome: "+p1.sobrenome);
+        System.out.println("Nome: "+p1.idade);
+
+        System.out.println("***********Dados pessoa 2************");
+        System.out.println("Nome: "+p2.nome);
+        System.out.println("Nome: "+p2.sobrenome);
+        System.out.println("Nome: "+p2.idade);
     }
 }
