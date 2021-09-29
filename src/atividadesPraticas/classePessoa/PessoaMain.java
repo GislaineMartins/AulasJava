20f4e46aa68deb2bce3662f1f38489e38fb1f6a2
Gislaine Amancio <gimcjf@gmail.com>
2021-09-29 14:44:19 -0300

    Criação de 2 objetos do tipo pessoa

--- src/atividadesPraticas/classePessoa/PessoaMain.java
+++ src/atividadesPraticas/classePessoa/PessoaMain.java
@@ -1,4 +1,16 @@
 package atividadesPraticas.classePessoa;
 
 public class PessoaMain {
+    public static void main(String[] args) {
+
+        Pessoa p1 = new Pessoa();
+        p1.nome = "Carlos";
+        p1.sobrenome = "Ferreira";
+        p1.idade = 30;
+
+        Pessoa p2 = new Pessoa();
+        p2.nome = "Rodrigo";
+        p2.sobrenome = "Souza";
+        p2.idade = 28;
+    }
 }
