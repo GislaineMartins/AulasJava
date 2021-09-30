4a0f95345c43c70ea7b102faafad56d3f8d0bdd1
Gislaine Amancio <gimcjf@gmail.com>
2021-09-30 13:10:49 -0300

    testes dos metodos criados

--- src/atividadesPraticas/atv16/CalculadoraTaxas.java
+++ src/atividadesPraticas/atv16/CalculadoraTaxas.java
@@ -3,11 +3,23 @@ package atividadesPraticas.atv16;
 public class CalculadoraTaxas {
 
     private double valor;
+    private int cont;
 
     public double taxaTransacaoBancaria(double valor){
         double taxa = (valor /100) * 0.001;
         double resultado = valor - taxa;
         return  resultado;
+
+    }
+
+    public double taxaDeSaque(double valor){
+        cont++;
+        if(cont == 5){
+            valor = valor - 1.30;
+            return valor;
+        }else{
+            return cont;
+        }
 
     }
 
--- src/atividadesPraticas/atv16/Main.java
+++ src/atividadesPraticas/atv16/Main.java
@@ -3,6 +3,21 @@ package atividadesPraticas.atv16;
 public class Main {
     public static void main(String[] args) {
 
+        CalculadoraTaxas c = new CalculadoraTaxas();
+        double v = c.taxaTransacaoBancaria(50);
+        System.out.println(v);
+        double contagem1 = c.taxaDeSaque(100);
+        System.out.println("valor: "+contagem1);
+        double contagem2 = c.taxaDeSaque(100);
+        System.out.println("valor: "+contagem2);
+        double contagem3 = c.taxaDeSaque(100);
+        System.out.println("valor: "+contagem3);
+        double contagem4 = c.taxaDeSaque(100);
+        System.out.println("valor: "+contagem4);
+        double contagem5 = c.taxaDeSaque(100);
+        System.out.println("valor: "+contagem5);
+
+
 
 
     }
