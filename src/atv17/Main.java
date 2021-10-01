1e3fa229ebb2557a8153c21751d15f54909eef8c
Gislaine Amancio <gimcjf@gmail.com>
2021-10-01 15:00:06 -0300

    teste do metodo calculoIOF

--- src/atv17/Main.java
+++ src/atv17/Main.java
@@ -5,8 +5,12 @@ import atividadesPraticas.atv16.CalculadoraTexas;
 public class Main {
     public static void main(String[] args) {
 
+        double result;
+
         CalculoImposto cal = new CalculoImposto();
-        double result = cal.calculoISS(5000);
+        result = cal.calculoISS(5000);
+        System.out.println("Valor do imposto ISS: "+ result);
+        result = cal.calculoIOF(5000);
         System.out.println("Valor do imposto ISS: "+ result);
 
     }
