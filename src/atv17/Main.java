c642ea3c7bff5f761ae826132ace86a16521cea6
Gislaine Amancio <gimcjf@gmail.com>
2021-10-01 15:23:22 -0300

    Testes de todos os metodos

--- src/atv17/Main.java
+++ src/atv17/Main.java
@@ -5,13 +5,17 @@ import atividadesPraticas.atv16.CalculadoraTexas;
 public class Main {
     public static void main(String[] args) {
 
-        double result;
 
         CalculoImposto cal = new CalculoImposto();
-        result = cal.calculoISS(5000);
-        System.out.println("Valor do imposto ISS: "+ result);
-        result = cal.calculoIOF(5000);
-        System.out.println("Valor do imposto ISS: "+ result);
+        double resultISS = cal.calculoISS(5000);
+        double resultIOF = cal.calculoIOF(5000);
+        double resultIR = cal.calculoIR(5000);
+
+
+        System.out.println("---------------Relatorio---------------");
+        System.out.println("Imposto calculo ISS: "+ resultISS);
+        System.out.println("Imposto calculo IOF: "+ resultIOF);
+        System.out.println("Imposto calculo IR: "+ resultIR);
 
     }
 }
