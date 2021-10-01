606c1e5c3a5e91a0c45ddb76f25874a398475528
Gislaine Amancio <gimcjf@gmail.com>
2021-10-01 14:57:14 -0300

    implementação do metodo calculoIOF

--- src/atv17/CalculoImposto.java
+++ src/atv17/CalculoImposto.java
@@ -3,15 +3,20 @@ package atv17;
 public class CalculoImposto {
 
     private double impostoISS = 0.04;
+    private double impostoIOF = 0.0038;
     private double valor;
+    private double resultado;
+
 
     public double calculoISS(double valor){
-        double resultado = valor * impostoISS;
+        resultado = valor * impostoISS;
         return resultado;
 
     }
 
-    public void calculoIOF(){
+    public double calculoIOF(double valor){
+        double resultado = valor * impostoIOF;
+        return resultado;
 
     }
 
