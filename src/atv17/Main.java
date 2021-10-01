30494ca7f0b06fc4d98ef24a88bfc58d94fcc3ee
Gislaine Amancio <gimcjf@gmail.com>
2021-10-01 14:50:35 -0300

    testando o metodo calculoISS

--- src/atv17/Main.java
+++ src/atv17/Main.java
@@ -1,7 +1,13 @@
 package atv17;
 
+import atividadesPraticas.atv16.CalculadoraTexas;
+
 public class Main {
     public static void main(String[] args) {
+
+        CalculoImposto cal = new CalculoImposto();
+        double result = cal.calculoISS(5000);
+        System.out.println("Valor do imposto ISS: "+ result);
 
     }
 }
