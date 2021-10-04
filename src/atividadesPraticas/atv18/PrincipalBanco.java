97c2e64ca8ec8b7f2311f1e3239afeeae09f32f4
Gislaine Amancio <gimcjf@gmail.com>
2021-10-04 13:45:59 -0300

    Teste da classe PrincipalBanco

--- src/atividadesPraticas/atv18/PrincipalBanco.java
+++ src/atividadesPraticas/atv18/PrincipalBanco.java
@@ -3,5 +3,22 @@ package atividadesPraticas.atv18;
 public class PrincipalBanco {
     public static void main(String[] args) {
 
+        ContaCorrente cc = new ContaCorrente();
+
+        cc.setAgencia(1234);
+        cc.setNumero(222222);
+        cc.setCodigoDoCliente(1212121212);
+        cc.setSaldo(5000);
+
+        int agencia = cc.getAgencia();
+        int numero = cc.getNumero();
+        int codigoDoCliente = cc.getCodigoDoCliente();
+        float saldo = cc.getSaldo();
+
+        System.out.println("Agencia: "+agencia);
+        System.out.println("Numero: "+numero);
+        System.out.println("Código do cliente: "+codigoDoCliente);
+        System.out.println("Saldo: "+saldo);
+
     }
 }
