5fbde606b2c2609997ccacb5b39790cee69f2eb2
Gislaine Amancio <gimcjf@gmail.com>
2021-10-04 13:33:30 -0300

    Criação das classes de teste PrincipalLoja e PrincipalBanco

---
+++ src/atividadesPraticas/atv18/PrincipalBanco.java
@@ -0,0 +1,7 @@
+package atividadesPraticas.atv18;
+
+public class PrincipalBanco {
+    public static void main(String[] args) {
+        
+    }
+}
---
+++ src/atividadesPraticas/atv18/PrincipalLoja.java
@@ -0,0 +1,9 @@
+package atividadesPraticas.atv18;
+
+public class PrincipalLoja {
+    public static void main(String[] args) {
+
+    }
+
+
+}
