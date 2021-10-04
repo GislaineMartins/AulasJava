906e13276c92c71ba7c6566d1e34f050567f5524
Gislaine Amancio <gimcjf@gmail.com>
2021-10-04 13:21:43 -0300

    Implementação dos metodos get e set da classe Produto

--- src/atividadesPraticas/atv18/Produto.java
+++ src/atividadesPraticas/atv18/Produto.java
@@ -1,2 +1,43 @@
-package atividadesPraticas.atv18;public class Produto {
+package atividadesPraticas.atv18;
+
+public class Produto {
+
+    private int codigo;
+    private String nome;
+    private String descricao;
+    private float valor;
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public int getCodigo() {
+        return this.codigo;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getNome() {
+        return this.nome;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public String getDescricao() {
+        return this.descricao;
+    }
+
+    public void setValor(float valor) {
+        this.valor = valor;
+    }
+
+    public float getValor() {
+        return this.valor;
+    }
+
+
 }
