7052c5ac070c1e4e2ff10debd83ea26f3d5c400b
Gislaine Amancio <gimcjf@gmail.com>
2021-10-04 13:31:09 -0300

    Implementação dos métodos get e set da classe ContaCorrente

--- src/atividadesPraticas/atv18/ContaCorrente.java
+++ src/atividadesPraticas/atv18/ContaCorrente.java
@@ -2,11 +2,40 @@ package atividadesPraticas.atv18;
 
 public class ContaCorrente {
 
-    private int agência;
+    private int agencia;
     private int numero;
     private int codigoDoCliente;
     private float saldo;
 
-    
+    public void setAgencia(int agencia) {
+        this.agencia = agencia;
+    }
 
+    public int getAgencia() {
+        return this.agencia;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+
+    public int getNumero() {
+        return this.numero;
+    }
+
+    public void setCodigoDoCliente(int codigoDoCliente) {
+        this.codigoDoCliente = codigoDoCliente;
+    }
+
+    public int getCodigoDoCliente() {
+        return this.codigoDoCliente;
+    }
+
+    public void setSaldo(float saldo) {
+        this.saldo = saldo;
+    }
+
+    public float getSaldo() {
+        return this.saldo;
+    }
 }
