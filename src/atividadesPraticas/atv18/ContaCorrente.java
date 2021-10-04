a563273c3c1d08002ff85f23b1656dc012abe3a1
Gislaine Amancio <gimcjf@gmail.com>
2021-10-04 13:26:27 -0300

    Criação da classe ContaCorrente e atributos

---
+++ src/atividadesPraticas/atv18/ContaCorrente.java
@@ -0,0 +1,12 @@
+package atividadesPraticas.atv18;
+
+public class ContaCorrente {
+
+    private int agência;
+    private int numero;
+    private int codigoDoCliente;
+    private float saldo;
+
+    
+
+}
