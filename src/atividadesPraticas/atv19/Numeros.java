91c1336537746e77f2b4f1275cdeca8ae58cec61
Gislaine Amancio <gimcjf@gmail.com>
2021-10-04 17:38:14 -0300

    Criação da classe PessoaFísica

--- src/atividadesPraticas/atv19/Numeros.java
+++ src/atividadesPraticas/atv19/Numeros.java
@@ -1,2 +1,23 @@
-package atividadesPraticas.atv19;public class Numeros {
+package atividadesPraticas.atv19;
+
+public class Numeros {
+    public static void main(String[] args) {
+
+        int[] numeros = new int[10];
+
+        numeros[0] = 10;
+        numeros[1] = 20;
+        numeros[2] = 30;
+        numeros[3] = 40;
+        numeros[4] = 50;
+        numeros[5] = 60;
+        numeros[6] = 70;
+        numeros[7] = 80;
+        numeros[8] = 90;
+        numeros[9] = 100;
+
+        for (int i=0; i<numeros.length; i++){
+            System.out.println("Numero "+ i + " = " + numeros[i]);
+        }
+    }
 }
---
+++ src/atividadesPraticas/atv19/PessoaFísica.java
@@ -0,0 +1,58 @@
+package atividadesPraticas.atv19;
+
+public class PessoaFísica {
+    private int código;
+    private String nome;
+    private String sobrenome;
+    private int idade;
+    private String rg;
+    private String cpf;
+
+    public int getCódigo() {
+        return código;
+    }
+
+    public void setCódigo(int código) {
+        this.código = código;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getSobrenome() {
+        return sobrenome;
+    }
+
+    public void setSobrenome(String sobrenome) {
+        this.sobrenome = sobrenome;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public String getRg() {
+        return rg;
+    }
+
+    public void setRg(String rg) {
+        this.rg = rg;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+}
