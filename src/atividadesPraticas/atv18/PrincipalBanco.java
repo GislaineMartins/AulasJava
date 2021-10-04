f0d9b63d8a839682146cc0e45f1876650d772637
Gislaine Amancio <gimcjf@gmail.com>
2021-10-04 13:39:29 -0300

    Teste da classe Produto

--- src/atividadesPraticas/atv18/PrincipalBanco.java
+++ src/atividadesPraticas/atv18/PrincipalBanco.java
@@ -2,6 +2,6 @@ package atividadesPraticas.atv18;
 
 public class PrincipalBanco {
     public static void main(String[] args) {
-        
+
     }
 }
--- src/atividadesPraticas/atv18/PrincipalLoja.java
+++ src/atividadesPraticas/atv18/PrincipalLoja.java
@@ -3,6 +3,25 @@ package atividadesPraticas.atv18;
 public class PrincipalLoja {
     public static void main(String[] args) {
 
+        Produto produto = new Produto();
+        produto.setCodigo(1);
+        produto.setNome("TV");
+        produto.setDescricao("Smart TV LED 55'' ");
+        produto.setValor(1500.00f);
+
+        int codigo = produto.getCodigo();
+        String nome = produto.getNome();
+        String descricao = produto.getDescricao();
+        float valor = produto.getValor();
+
+        System.out.println("Codigo: "+ codigo);
+        System.out.println("Nome: "+ nome);
+        System.out.println("Descrição: "+ descricao);
+        System.out.println("Valor: "+ valor);
+
+
+
+
     }
 
 
