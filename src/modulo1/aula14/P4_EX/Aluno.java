d8c0b4503c6575ee5184e12de60a2fbc82775aec
Gislaine Amancio <gimcjf@gmail.com>
2021-11-03 18:08:29 -0300

    Adicionando pasta P4_EX

---
+++ src/modulo1/aula14/P4_EX/Aluno.java
@@ -0,0 +1,20 @@
+package modulo1.aula14.P4_EX;
+
+public class Aluno extends Pessoa{
+    private String curso;
+    private String turma;
+
+    public void setCurso(String curso) {
+        this.curso = curso;
+    }
+    public String getCurso() {
+        return curso;
+    }
+
+    public void setTurma(String turma) {
+        this.turma = turma;
+    }
+    public String getTurma() {
+        return turma;
+    }
+}
